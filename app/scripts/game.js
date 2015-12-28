60cc8f7ae9e60d4944e89940edbcad2225894920
xalava <xavier.lavayssiere@gmail.com>
2015-12-28 09:21:07 +0100

    jeuFonctionnel

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,16 +1,16 @@
 // ** Models **
-
+// idee: timer
 //small utility function
 function aleaJactaEst(){
 	return Math.floor((Math.random() * 100) + 1);
 }
 
-var Player = function(id, color, name) {
+var Player = function(id, name, color) {
 	this.id = id;
 	this.name = name;
 	this.color = color;
 	this.nbCities = 0;
-	this.nbRsv = 0;
+	this.nbRsv = 5;
 	if(id === 0) {
 		this.alive=false;
 	} else {
@@ -19,14 +19,17 @@ var Player = function(id, color, name) {
 }
 
 
-var Game = function (nbPlayers, water, cities, mapX, mapY, players) {
-  console.log("game created");
+var Game = function (nbPlayers, water, cities, mapX, mapY, players, squareSize) {
   this.nbPlayers = nbPlayers;
   this.currentPlayer = 1;
   this.players = players;
   this.mapX = mapX;
   this.mapY = mapY;
-  this.map = this.generateMap(mapX, mapY, water, cities);
+  this.squareSize = squareSize;
+  this.board = this.generateMap(mapX, mapY, water, cities, squareSize, nbPlayers);
+  this.mTxt ="Welcome to Supply Line"
+  this.mType="success"
+  console.log("game created");
 };
 
 Game.WATER = -1;
@@ -35,13 +38,13 @@ Game.CITY = 1;
 Game.HQ = 2;
 
 
-Game.prototype.generateMap = function(mapX, mapY, water, cities) {
+Game.prototype.generateMap = function(mapX, mapY, water, cities, squareSize, nbPlayers) {
 	var map = [];
 	// init
 
 	for (var j = mapY - 1; j >= 0; j--) {
 		map[j] = []; 
-		//! nb it's [line][col]
+		//! nb it's [row][col]
 		for (var i = mapX - 1; i >=0; i--) {
 			//Default (and avoid JS errors)
 
@@ -50,8 +53,10 @@ Game.prototype.generateMap = function(mapX, mapY, water, cities) {
 				feature: 0,
 				supplied: true,// essential for first turn
 				occupied: false,
-				x:i,// I know weird, best solution so far,
-				y:j // open for ideas to retreive their location and place them correctly with v-for
+				boardCol:i,
+				boardRow:j,
+				x:i*squareSize,// temp // I know weird, best solution so far,
+				y:j*squareSize// open for ideas to retreive their location and place them correctly with v-for
 			}
 
 			var aleat = aleaJactaEst();
@@ -63,65 +68,106 @@ Game.prototype.generateMap = function(mapX, mapY, water, cities) {
 				if (aleat > (100 - cities)){
 					map[j][i].feature = 1;
 				}
+				//todo adapt to severaly players
+				switch (nbPlayers){
+					case 2:
+						var limit = Math.floor(	this.mapX* 2/5)
+						if (i < limit) {
+							map[j][i].owner = 1;
+						} else if (i > mapX - limit) {
+							map[j][i].owner = 2;
+						}	
+
+						break;
+					case 3:
+						//todo
+
+						break;
+					case 4:
+						break;
+					default:
+						//todo
+						break;
+
+
+				}
+
+ 
 			}
 
 			//owner
-			if (i <this.mapX/3) {
-				map[j][i].owner = 1;
-			} else if (i > mapX * 2/3) {
-				map[j][i].owner = 2;
-			} else {
-				map[j][i].owner = 0;
 
-			}	 
 		}
 	} //end double for
 
 	//placing HQ
-	map[0][0].feature = 2;
-	//map[mapX - 1][mapY - 1].feature = 2;
+	map[1][1].feature = Game.HQ;
+
+	map[mapY -2][mapX -2].feature = Game.HQ;
 
 	return map;
 
 }
+Game.prototype.message = function(txt, type) {
+	this.mTxt= txt;
+	this.mType=type;
+	//success, info, warning, danger
+}
+
 
 Game.prototype.squareAction = function(x, y) {
-	var cs = this.map[x][y]
+	var cs = this.board[y][x]
 	if (cs.feature === Game.WATER ) {
 		//throw this is water!
 	} else {
 		if (cs.owner === this.currentPlayer){
 			if (cs.supplied){
-				if(cs.occupied === false && this.players[this.currentPlayer].nbRsv > 0){
-					cs.occupied = true;
-					this.players[this.currentPlayer].nbRsv -= 1
+				this.message("","")
+				if(cs.occupied === false){
+					if(this.players[this.currentPlayer].nbRsv > 0){
+						cs.occupied = true;
+						this.players[this.currentPlayer].nbRsv -= 1
+					} else {
+						//throw not enought troupe
+					}
 				} else {
 					cs.occupied = false;
 					this.players[this.currentPlayer].nbRsv += 1
 				}
 			} else {
 				//TODO throw unsupplied territory
+				this.message("unsupplied territory","warning")
 			}
 		} else {
 			if( this.players[this.currentPlayer].nbRsv > 0){
-				//Try to conquer the square
-				var difficulty = 10;
-				difficulty += cs.feature * 20
-				if (cs.occupied){difficulty += 20}
-				if (cs.owner !== 0){difficulty += 20}
+				if(this.board[Math.max(y-1,0)][x].owner == this.currentPlayer || this.board[Math.min(y+1,this.mapY)][x].owner == this.currentPlayer || this.board[y][Math.max(x-1,0)].owner == this.currentPlayer  || this.board[y][Math.min(x+1,this.mapX)].owner == this.currentPlayer ){
+					//Try to conquer the square
+					var difficulty = 10;
+					difficulty += cs.feature * 20
+					if (cs.occupied){difficulty += 20}
+					if (cs.owner !== 0){difficulty += 20}
 
-				// TODO: lower difficulty if surrounding
-				//maybe lower diff if unsupplied
-				if (difficulty<aleaJactaEst()){
-					cs.owner= this.currentPlayer
-					cs.occupied = true
-					cs.supplied = true
+					// TODO: lower difficulty if surrounding
+					//maybe lower diff if unsupplied
+					if (difficulty<aleaJactaEst()){
+						cs.owner= this.currentPlayer
+						cs.occupied = true
+						cs.supplied = true
+						this.message("New territory!","success");
+					} else {
+						this.message("Battle lost","warning");
+					}
+					
+					//removing a troup anyway
+					this.players[this.currentPlayer].nbRsv -= 1
+				} else {
+					this.message("You need to be adjacent to your territories","warning")
 				}
-				
-				//removing a troup anyway
-				this.players[this.currentPlayer].nbRsv -= 1
+
 			}else {
 				//TRrow not enough soldier
+				this.message("No troups left","warning");
+
 			}
 
 
@@ -136,6 +182,14 @@ Game.prototype.updateSupplies = function() {
 	//todo (not critical)
 
 }
+Game.prototype.countCities = function() {
+	//todo (not critical)
+
+}
+Game.prototype.newRecruits = function() {
+	//todo (not critical)
+
+}
 
 
 Game.prototype.changePlayer = function(){
@@ -144,11 +198,15 @@ Game.prototype.changePlayer = function(){
 	} else {
 		this.currentPlayer = this.currentPlayer + 1
 	}
+	this.players[currentPlayer].nbRsv +=3 ;
 }
 
 Game.prototype.endTurn = function() {
 	this.updateSupplies();
 	this.changePlayer();
+	this.message("New turn","info");
 
+	this.countCities();
+	this.newRecruits();
 
 }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,31 @@
 console.log("Supply Line on the console");
 
-
+var daGame
+var game
 
 window.onload = function() {
     var backgroundAudio=document.getElementById("bgAudio");
     backgroundAudio.volume=0.1;
    // document.getElementById('bgAudio').play()
+   launchGame();
 }
 
 
-// test objects
+// testing objects
 
 var settings = {
 	nbPlayers: 2,
-	water: 15,
-	size: 20,
-	mapX: 30,
-	mapY: 20,
+	water: 10,
+	squareSize: 25,
+	mapX: 16,
+	mapY: 14,
 	cities: 3
 }
 
 var playersList = [
-   			{name: "Neutral", color:"#333", nbCities:0,nbRsv:0, alive: false },
-   			{name: "Lefty", color:"#234", nbCities:0,nbRsv:0, alive: true },
-   			{name: "Righty", color:"#AEC", nbCities:0,nbRsv:0, alive: true }
+   			{name: "Neutral", color:"#333"},
+   			{name: "Lefty", color:"#F34"},
+   			{name: "Righty", color:"#AEC"}
    		]
 
 
@@ -33,26 +35,105 @@ function launchGame() {
 		players[i] = new Player(i,playersList[i].name, playersList[i].color)
 	};
 
-	var daGame = new Game(settings.nbPlayers,settings.water,settings.cities,settings.mapX,settings.mapY,players);
+	daGame = new Game(settings.nbPlayers,settings.water,settings.cities,settings.mapX,settings.mapY,players, settings.squareSize);
+	
+	console.log(daGame);
+	// Vue.js
 
 
-var game = new Vue({
-   el: '#game',
-   data: {
-   		map: daGame.Board,
-   		players: players,
-   		message: toDisplay,
-   		
-   }
 
+	game = new Vue({
+	   el: '#game',
+	   data: daGame,
 
-});
+	   computed: {
+	       currentPlayerName : function () {
+	         return this.players[this.currentPlayer].name;
+	       },
+	       daPlayer : function () {
+	         return this.players[this.currentPlayer];
+	       },
+	       mapWidth : function () {
+	         return this.mapX * this.squareSize;
+	       },
+	       mapHeight : function () {
+	         return this.mapY * this.squareSize;
+	       },
+	       squarelocation : function () {
+	         return this.mapY * this.squareSize;
+	       },
+	       squares : function () {
+	         	return this.board.reduce(function(a, b) {
+	       		return a.concat(b);
+	     		});
+	       	},
+	       rows : function () {
+	         	return this.board;
+	       	}
+	    },
+		components: {
+		// <-component> will only be available in Parent's template
+			'square-component': {
+		     	props: {
+		     		sq: Object
+		     	},
+	      		template:'#square-template',
+				data: function () {
+				    return this.sq;
+				  }, // !??
+
+	      		computed: {
+	      			isWater : function () {
+	      				if (this.feature == -1){
+	      					return true;
+	      				} else {
+	      					return false;
+	      				}
+	     			},
+	     			isCity : function () {
+	      				if (this.feature == 1){
+	      					return true;
+	      				} else {
+	      					return false;
+	      				}
+	     			},
+	     			isHQ : function () {
+	      				if (this.feature == 2){
+	      					return true;
+	      				} else {
+	      					return false;
+	      				}
+	     			},
+	     			playerColor : function () {
+	     				//should be passed from parent
+	     				if (this.owner>0) {
+	     				return daGame.players[this.owner].color;
+	     			}
+
+	     			},
+	     		methods: {
+	     			sqAction: function() {
+
+	     				daGame.squareAction(this.boardCol,this.boardRow);
+	     			}
+	     		}
+
+	     		}
+	     	}// End square
+		} // End components
+
+	}); // vue object
 
 // todo shufflemap function
 
 
 }
 
+function endTurn() {
+	daGame.endTurn();
+	
+
+}
 
 
 
